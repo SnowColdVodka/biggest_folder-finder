b5e123f4c0662daadc73cfb2b2aa89bf328b3b6d
daniil_epikhov <depikhov0@gmail.com>
2021-11-02 23:00:46 +0300

    dev

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+    public static void main(String[] args) {
+        String folderPath = "C:\\Users\\snowc\\Videos";
+        File file = new File(folderPath);
+
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        //System.out.println(getFolderSize(file));
+        long end = System.currentTimeMillis() - start;
+        System.out.println(end);
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+
+        return sum;
+    }
+}
